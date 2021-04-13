3556e79aa0ff37d1d624be2c93e05f6f4f04ddec
Lucas Hijman <0968128@hr.nl>
2021-04-13 13:19:00 +0200

    guess -1, +1
    made the code appropriate for showing my guess -1 and +1

--- cluster.py
+++ cluster.py
@@ -11,7 +11,7 @@ data = Machine_Learning_Data(glob.student_number)
 # Haal clustering data op
 kmeans_training = data.clustering_training()
 
-# extract de x waarden
+# Extract de x waarden
 X = glob.extract_from_json_as_np_array("x", kmeans_training)
 
 # Slice kolommen voor plotten (let op, dit is de y voor de y-as, niet te verwarren met een y van de data)
@@ -21,26 +21,32 @@ y = X[...,1]
 # Teken als zwarte punten
 for i in range(len(x)):
     plt.plot(x[i], y[i], "k.")
-    
+
+plt.xlim(-40, 80)
+plt.ylim(-20, 140)
 plt.show()
 
 # ontdek de clusters mbv kmeans en teken een plot met kleurtjes
-colors = ["red", "blue", "green"]
-colors.append("yellow")
-# colors.append("purple")
+cluster_amount_guess = 4
+colors = ["red", "blue", "green", "yellow", "orange", "pink", "purple"]
 
-kmeans = KMeans(len(colors))
+kmeans = KMeans(cluster_amount_guess)
 kmeans.fit(X)
 clusters = kmeans.cluster_centers_
 y_km = kmeans.fit_predict(X)
 
-# Teken de punten in kleur, zodat er clusters te zien zijn
-for i in range(len(colors)):
+# Teken de punten in kleur, zodat het geschatte aantal clusters te zien is
+for i in range(cluster_amount_guess):
     plt.scatter(X[y_km == i, 0], X[y_km == i, 1], color=colors[i])
 
-# Teken de clusters met centroids
-for i in range(len(colors)):
-    plt.scatter(X[y_km == i, 0], X[y_km == i, 1], color=colors[i])
-    plt.scatter(kmeans.cluster_centers_[i][0], kmeans.cluster_centers_[i][1], c="black")
+plt.xlim(-40, 80)
+plt.ylim(-20, 140)
+plt.show()
 
+# Teken de centroids voor mijn gok
+for i in range(cluster_amount_guess):
+    plt.scatter(kmeans.cluster_centers_[i][0], kmeans.cluster_centers_[i][1], color=colors[i])
+
+plt.xlim(-40, 80)
+plt.ylim(-20, 140)
 plt.show()
